00fb4224b809570c7b216fa7128bc2b8a6cfd5b9
phucnh1993 <phucnh1993@gmail.com>
2021-04-13 22:32:57 +0700

    Tối ưu code

--- proliferation/src/main/java/requests/FilterRequest.java
+++ proliferation/src/main/java/requests/FilterRequest.java
@@ -1,5 +1,13 @@
 package requests;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import configs.ConstantConfig;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +27,48 @@ public class FilterRequest {
 	
 	public int getOffset() {
 		return (pageIndex - ConstantConfig.PAGE_INDEX_DEFAULT) * pageSize;
+	}
+	
+	public <T> List<Predicate> CreateListPredicate(CriteriaBuilder cb, Root<T> root, Class<T> myClass) {
+		List<Predicate> predicates = new ArrayList<Predicate>();
+		if (this.keyWords != null && !this.keyWords.trim().isEmpty()) {
+			Predicate condition = cb.like(root.get("name"), this.keyWords + "%");
+			predicates.add(condition);
+		}
+		if (this.from != null && !this.from.trim().isEmpty()) {
+			Predicate condition = cb.greaterThanOrEqualTo(root.get("ModifiedOn"), this.from);
+			predicates.add(condition);
+		}
+		if (this.to != null && !this.to.trim().isEmpty()) {
+			Predicate condition = cb.lessThanOrEqualTo(root.get("ModifiedOn"), this.to);
+			predicates.add(condition);
+		}
+		return predicates;
+	}
+	
+	public <T> CriteriaQuery<T> UseListPredicate(CriteriaQuery<T> query, List<Predicate> predicates) {
+		if (predicates.size() > 0) {
+			predicates.forEach((element) -> {
+				query.where(element);
+			});
+		}
+		return query;
+	}
+	
+	public <T> CriteriaQuery<Long> CreateCount(CriteriaBuilder cb, Class<T> myClass) {
+		CriteriaQuery<Long> query = cb.createQuery(Long.class);
+		Root<T> root = query.from(myClass);
+		List<Predicate> predicates = CreateListPredicate(cb, root, myClass);
+		query.select(cb.count(root));
+		query = UseListPredicate(query, predicates);
+		return query;
+	}
+	
+	public <T> CriteriaQuery<T> CreateQuery(CriteriaBuilder cb, Class<T> myClass) {
+		CriteriaQuery<T> query = cb.createQuery(myClass);
+		Root<T> root = query.from(myClass);
+		List<Predicate> predicates = CreateListPredicate(cb, root, myClass);
+		query = UseListPredicate(query, predicates);
+		return query;
 	}
 }
--- proliferation/src/main/java/services/language/LanguageQuery.java
+++ proliferation/src/main/java/services/language/LanguageQuery.java
@@ -7,9 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Service;
 
@@ -25,53 +22,20 @@ public class LanguageQuery {
 	@PersistenceContext
 	final EntityManager entityManager;
 
+	public Long count(FilterRequest filter) {
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		return entityManager.createQuery(filter.CreateCount(cb, Language.class)).getSingleResult();
+	}
+
 	public ResultData<List<Language>> getAll(FilterRequest filter) {
 		ResultData<List<Language>> result = new ResultData<List<Language>>();
 		result.setData(new ArrayList<Language>());
 		result.setErrorCode(ConstantConfig.SUCCESS);
 		result.setMessage(ConstantConfig.StatusMessage.get(ConstantConfig.SUCCESS));
 		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Language> criteriaQuery = cb.createQuery(Language.class);
-		CriteriaQuery<Long> countQuery = cb.createQuery(Long.class);
-		List<Predicate> predicates = new ArrayList<Predicate>();
-		// Tạo root của câu query
-		Root<Language> rootCount = countQuery.from(Language.class);
-		Root<Language> root = criteriaQuery.from(Language.class);
-//		if (filter.getKeyWords() != null && !filter.getKeyWords().trim().isEmpty()) {
-//			Predicate condition = cb.like(countQuery.from(Language.class).get("name"), filter.getKeyWords() + "%");
-//			predicates.add(condition);
-//		}
-		if (filter.getKeyWords() != null && !filter.getKeyWords().trim().isEmpty()) {
-			Predicate condition = cb.like(cb.createQuery(Long.class).from(Language.class).get("name"), filter.getKeyWords() + "%");
-			predicates.add(condition);
-		}
-		if (filter.getFrom() != null && !filter.getFrom().trim().isEmpty()) {
-			Predicate condition = cb.greaterThanOrEqualTo(countQuery.from(Language.class).get("ModifiedOn"), filter.getFrom());
-			predicates.add(condition);
-		}
-		if (filter.getTo() != null && !filter.getTo().trim().isEmpty()) {
-			Predicate condition = cb.lessThanOrEqualTo(countQuery.from(Language.class).get("ModifiedOn"), filter.getTo());
-			predicates.add(condition);
-		}
-
-		// Tạo câu truy vấn
-		CriteriaQuery<Language> select = criteriaQuery.select(root);
-
-		// Đếm giá trị của câu query
-		countQuery.select(cb.count(rootCount));
-
-		// countQuery.where(criteriaBuilder.like(countQuery.from(Language.class).get("Name"),
-		// filter.getKeyWords() + "%"));
-		if (predicates.size() > 0) {
-			predicates.forEach((element) -> {
-				countQuery.where(element);
-			});
-		}
-
-		result.setTotal(entityManager.createQuery(countQuery).getSingleResult());
+		result.setTotal(count(filter));
 		if (result.getTotal() > 0) {
-
-			TypedQuery<Language> typedQuery = entityManager.createQuery(select).setFirstResult(filter.getOffset())
+			TypedQuery<Language> typedQuery = entityManager.createQuery(filter.CreateQuery(cb, Language.class)).setFirstResult(filter.getOffset())
 					.setMaxResults(filter.getPageSize());
 			result.setData(typedQuery.getResultList());
 		}
