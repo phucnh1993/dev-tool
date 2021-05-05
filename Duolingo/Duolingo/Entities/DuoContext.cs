250cc91a7e2cee32e3426bc0602ee56b9d6b84ea
phucnh1993 <phucnh1993@gmail.com>
2021-05-05 22:27:12 +0700

    Update my sql

--- Duolingo/Duolingo/Entities/DuoContext.cs
+++ Duolingo/Duolingo/Entities/DuoContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.EntityFramework;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Duolingo.Entities {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class DuoContext : DbContext {
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DuoContext() : base("DuoDb") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Topic>().MapToStoredProcedures();
+            modelBuilder.Entity<Question>().MapToStoredProcedures();
+        }
     }
 }
